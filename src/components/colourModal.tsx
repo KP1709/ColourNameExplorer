b02e5e22727bef5a6001d9f6b6171f330d559b51
Kareena Patel <kareenapat17@gmail.com>
2024-10-10 16:46:20 +0100

    Get and display  HSL and RGB codes from API

--- src/components/colourModal.tsx
+++ src/components/colourModal.tsx
@@ -1,23 +1,46 @@
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { colourObject } from "../data/colourObject";
 import "../styles/modal.css"
 
 type ModalProps = {
     open: boolean,
     onClose: () => void,
-    selectedColour: string
+    hexValue: string | undefined
 }
 
-export default function ColourModal({ open, onClose, selectedColour }: ModalProps) {
-    if (!open) return null
-    const chosenColourHexCode = colourObject.find(colour => colour.colourName === selectedColour)?.hexColour
+export default function ColourModal({ open, onClose, hexValue }: ModalProps) {
+    const [hslValue, setHslValue] = useState("")
+    const [rgbValue, setRgbValue] = useState("")
+
+    useEffect(() => {
+        async function getColourCodes() {
+            if (hexValue === undefined) return null
+            else
+                try {
+                    const response = await fetch(`https://www.thecolorapi.com/id?hex=${hexValue.substring(1)}&format=json`)
+                    const data = await response.json()
+                    setHslValue(data.rgb.value)
+                    setRgbValue(data.hsl.value)
+                }
+                catch (err) {
+                    setHslValue('Unknown')
+                    setRgbValue('Unknown')
+                    console.error(err)
+                }
+        }
+        getColourCodes()
+    }, [hexValue])
+
+    if (!open) return null // React doesn't like early returns (causes internal error)
 
     return createPortal(
         <>
             <div className="modal-overlay"></div>
             <div className="modal">
                 <button onClick={onClose}>Close</button>
-                <p>Hex code: {chosenColourHexCode}</p>
+                <p>Hex code: {hexValue}</p>
+                <p>RGB code: {rgbValue}</p>
+                <p>HSL code: {hslValue}</p>
             </div>
         </>,
         document.getElementById('portal-root')! // ! - Non-Null assertion operation
