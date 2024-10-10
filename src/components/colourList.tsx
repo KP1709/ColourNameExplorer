47f31cc7247509b3ece48ad9d3bbc06c3bea749c
Kareena Patel <kareenapat17@gmail.com>
2024-10-10 16:44:07 +0100

    Hex colour search done at higher level

--- src/components/colourList.tsx
+++ src/components/colourList.tsx
@@ -10,20 +10,20 @@ function ColourList({ enteredColour, isLoading }: ColourList) {
     const [isOpen, setIsOpen] = useState(false)
     const [selectedColour, setSelectedColour] = useState("")
     const queriedColours = colourObject.filter(colour => colour.colourName.toLowerCase().includes(enteredColour, 0))
+    const chosenColourHexCode = colourObject.find(colour => colour.colourName === selectedColour)?.hexColour
 
     return (
         <>
             <ul style={{ opacity: isLoading ? 0.5 : 1 }}>
                 {queriedColours.map(colours =>
-                    <li key={uuid()} onClick={() => {setIsOpen(true); setSelectedColour(colours.colourName)}}>{colours.colourName}
+                    <li key={uuid()} onClick={() => { setIsOpen(true); setSelectedColour(colours.colourName) }}>{colours.colourName}
                         <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill={colours.hexColour} d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z" />
                         </svg>
                     </li>
                 )}
             </ul>
-
-            <ColourModal selectedColour={selectedColour} open={isOpen} onClose={() => setIsOpen(false)} />
+            <ColourModal hexValue={chosenColourHexCode} open={isOpen} onClose={() => setIsOpen(false)} />
         </>
     )
 }
